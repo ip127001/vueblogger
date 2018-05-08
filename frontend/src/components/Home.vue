fee5cec86b411bc1b3c8c097a3b9340108272cb1
Rohit Kumawat <rkumawat125@gmail.com>
2018-05-08 14:02:54 +0530

    add post form added

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <div class="container-fluid">
+          <div class="row">
+              <div class="col-xs-12">
+                  Welcome to VueTube
+              </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+    
+</script>
